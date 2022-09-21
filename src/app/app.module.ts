d79bbeac35cde34775039c3857a121909db5f91f
jvanhamlin <jvanhamlin@hitpromo.net>
2022-09-21 11:44:29 -0400

    Add initial static layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LogoComponent } from './logo/logo.component';
+import { SearchComponent } from './search/search.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    SearchComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
